f71146eff95b83791681daa76a2b27d3b0a275db
Aurel Pop <47039993+aurel-pop@users.noreply.github.com>
2023-02-08 14:24:02 +0100

    added search results

--- tracker.py
+++ tracker.py
@@ -10,7 +10,7 @@ store = {}
 search_lists = search.split()
 amazon_link = "https://amazon.de"
 
-url = amazon_link+'/s?k='+'+'.join(search_lists)+'aurel0ec-21'
+url = amazon_link+'/s?k='+'+'.join(search_lists)
 
 def tracker(random_header):
     # Make a GET request to the Amazon URL using the "requests" library
@@ -26,12 +26,15 @@ def tracker(random_header):
     # Find the first "span" tag with the specified class attribute
     tag_2 = soup('span',{'class':'a-price-whole'})
 
+    print("Search results for: ",search)
+
     # Use "zip" to iterate over both "tag" and "tag_2" in parallel
     for i,j in zip(tag,tag_2):
+        print(i.text,j.text)
         # Check if the "search" term appears in the text of the first "span" tag
         if search.lower() in (i.text).lower():
             # If it does, print the item name and price
-            print("{} || price: {} Rs".format(i.text,j.text))
+            print("{} || price: {} €".format(i.text,j.text))
 
             # Store the item name and price in a dictionary called "store"
             store[str(i.text)]=j.text
