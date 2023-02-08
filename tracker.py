66fc9f7a9486f3e8c927ec74f01c9eccb17162f8
Aurel Pop <47039993+aurel-pop@users.noreply.github.com>
2023-02-08 13:55:37 +0100

    added comments to tracker function

--- tracker.py
+++ tracker.py
@@ -10,21 +10,40 @@ store = {}
 search_lists = search.split()
 amazon_link = "https://amazon.de"
 
-url = amazon_link+'/s?k='+'+'.join(search_lists)+'&ref=nb_sb_noss'
+url = amazon_link+'/s?k='+'+'.join(search_lists)+'aurel0ec-21'
 
 def tracker(random_header):
+    # Make a GET request to the Amazon URL using the "requests" library
+    # The "headers" parameter is set to the value of "random_header"
     url_open = requests.get(url,headers = random_header)
+
+    # Use BeautifulSoup to parse the HTML content of the page
     soup = BeautifulSoup(url_open.content,'html.parser')
+
+    # Find the first "span" tag with the specified class attribute
     tag = soup('span',{'class':'a-size-medium a-color-base a-text-normal'})
+
+    # Find the first "span" tag with the specified class attribute
     tag_2 = soup('span',{'class':'a-price-whole'})
+
+    # Use "zip" to iterate over both "tag" and "tag_2" in parallel
     for i,j in zip(tag,tag_2):
+        # Check if the "search" term appears in the text of the first "span" tag
         if search.lower() in (i.text).lower():
+            # If it does, print the item name and price
             print("{} || price: {} Rs".format(i.text,j.text))
+
+            # Store the item name and price in a dictionary called "store"
             store[str(i.text)]=j.text
+
+            # Check if the current item name already exists in the "store" dictionary
             for a,b in store.items():
                 if str(i.text)==a:
+                    # If it does, compare the current price to the previous price
                     if j.text<b:
+                        # If the current price is lower, show a toast notification using the "toaster" library
                         toaster.show_toast("Amazon Deal",i.text+" || Price"+j.text)
+
 
 while True:
     user = UserAgent()
